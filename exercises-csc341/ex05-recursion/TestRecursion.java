04cda402ed64ad78b2f4453e90e05a9a2b33d649
aad3m <aliadem0205@gmail.com>
2024-09-30 15:22:06 -0500

    TestRecursion.java edits

--- exercises-csc341/ex05-recursion/TestRecursion.java
+++ exercises-csc341/ex05-recursion/TestRecursion.java
@@ -1,5 +1,7 @@
 class TestRecursion {
 
+	// Ali, AbdiRashid, Armand
+
 	public static void main(String[] args) {
 
 		int[] A = { 5, 4, 3, 2, 1 };
@@ -20,23 +22,37 @@ class TestRecursion {
 	/** Create a recursive algorithm to sum up to index n **/
 	// The recursive call to sumUpTo should be modifying the "end" value
 	public static int sumUpTo(int[] A, int end) {
-		// placeholder until this is implemented
-		return 0;
+		if (end == 0) {
+			return 0;
+		}
+		else {
+			return A[end]+sumUpTo(A,end-1);
+		}
 	}
 
 	/** Create a recursive algorithm to sum from start to end index */
 	// In this version, the recursive call should be modifying both start and end.
 	public static int sumRange(int[] A, int start, int end) {
-		// placeholder until this is implemented
-		return 0;
+		if (start >= end){
+			return 0;
+		}
+		else {
+			return A[start]+A[end]+sumRange(A,start+1,end-1);
+		}
+
 	}
 
 	/** Create a recursive algorithm to sum from start to end index */
 	// In this version, split the array into 2 subarrays.
 	// You should have 2 recursive calls.
 	public static int sumSubarray(int[] A, int start, int end) {
-		// placeholder until this is implemented
-		return 0;
+		int mid = (end+start)/2;
+		if (start == end) {
+			return 0;
+		}
+		else {
+			return sumSubarray(A,start+1,mid) + sumSubarray(A,mid,end-1);
+		}
 	}
 
 }
